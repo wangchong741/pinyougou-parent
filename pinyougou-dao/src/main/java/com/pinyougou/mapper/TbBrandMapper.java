ba7ec1192edfd311533669a711f13c820a9db0e8
wangchong741 <853967228@qq.com>
2019-01-15 13:57:19 +0800

    添加缓存依赖

--- pinyougou-dao/src/main/java/com/pinyougou/mapper/TbBrandMapper.java
+++ pinyougou-dao/src/main/java/com/pinyougou/mapper/TbBrandMapper.java
@@ -5,7 +5,11 @@ import java.util.Map;
 
 import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.pojo.TbBrandExample;
-
+/**
+ * 品牌接口
+ * @author wangchong
+ * 2019年1月15日
+ */
 public interface TbBrandMapper {
   
     List<TbBrand> selectByExample(TbBrandExample example);
