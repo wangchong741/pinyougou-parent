e547a2839d8f28e1eec2ce6d23c2241122e3654a
wangchong741 <853967228@qq.com>
2019-01-19 01:08:28 +0800

    搜索结果分页前台

--- pinyougou-search-web/src/main/webapp/js/controller/searchController.js
+++ pinyougou-search-web/src/main/webapp/js/controller/searchController.js
@@ -1,7 +1,7 @@
 app.controller('searchController',function($scope,searchService){	
 	
 	//定义搜索对象的结构
-	$scope.searchMap={'keywords':'','category':'','brand':'','spec':{} ,'price':''};
+	$scope.searchMap={'keywords':'','category':'','brand':'','spec':{} ,'price':'' ,'pageNo':1,'pageSize':40};
 	
 	
 	//添加搜索项，改变searchMap的值
@@ -28,10 +28,82 @@ app.controller('searchController',function($scope,searchService){
 	
 	//搜索
 	$scope.search=function(){
+		$scope.searchMap.pageNo= parseInt($scope.searchMap.pageNo) ;
 		searchService.search( $scope.searchMap ).success(
 			function(response){						
 				$scope.resultMap=response;//搜索返回的结果
+				
+				buildPageLabel();//构建分页栏
 			}
 		);	
 	}	
+	
+	
+	//构建分页标签(totalPages为总页数)
+	buildPageLabel=function(){
+		$scope.pageLabel=[];//新增分页栏属性		
+		var maxPageNo= $scope.resultMap.totalPages;//得到最后页码
+		var firstPage=1;//开始页码
+		var lastPage=maxPageNo;//截止页码		
+		
+		$scope.firstDot=true;//前面有点
+		$scope.lastDot=true;//后边有点		
+		
+		if($scope.resultMap.totalPages> 5){  //如果总页数大于5页,显示部分页码		
+			if($scope.searchMap.pageNo<=3){//如果当前页小于等于3
+				lastPage=5; //前5页
+				$scope.firstDot=false;//前面没点
+			}else if( $scope.searchMap.pageNo>=lastPage-2  ){//如果当前页大于等于最大页码-2
+				firstPage= maxPageNo-4;		 //后5页	
+				$scope.lastDot=false;//后边没点
+			}else{ //显示当前页为中心的5页
+				firstPage=$scope.searchMap.pageNo-2;
+				lastPage=$scope.searchMap.pageNo+2;			
+			}
+		}else{
+			$scope.firstDot=false;//前面无点
+			$scope.lastDot=false;//后边无点
+		}
+		
+		//循环产生页码标签				
+		for(var i=firstPage;i<=lastPage;i++){
+			$scope.pageLabel.push(i);				
+		}		
+	}
+	
+	
+	//根据页码查询
+	$scope.queryByPage=function(pageNo){
+		//页码验证
+		if(pageNo<1 || pageNo>$scope.resultMap.totalPages){
+			return;
+		}		
+		$scope.searchMap.pageNo=pageNo;			
+		$scope.search();
+	}
+	
+	
+	//判断当前页为第一页
+	$scope.isTopPage=function(){
+		if($scope.searchMap.pageNo==1){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	
+	//判断当前页是否未最后一页
+	$scope.isEndPage=function(){
+		if($scope.searchMap.pageNo==$scope.resultMap.totalPages){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	
+	
+	
+	
 });
