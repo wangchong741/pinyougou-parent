f97e38ae80f2b16094ff468c9acd2c25a8c22591
wangchong741 <853967228@qq.com>
2019-01-18 23:14:32 +0800

    完成价格区间搜索

--- pinyougou-search-web/src/main/webapp/js/controller/searchController.js
+++ pinyougou-search-web/src/main/webapp/js/controller/searchController.js
@@ -1,12 +1,12 @@
 app.controller('searchController',function($scope,searchService){	
 	
 	//定义搜索对象的结构
-	$scope.searchMap={'keywords':'','category':'','brand':'','spec':{}};
+	$scope.searchMap={'keywords':'','category':'','brand':'','spec':{} ,'price':''};
 	
 	
 	//添加搜索项，改变searchMap的值
 	$scope.addSearchItem=function(key,value){
-		if(key=='category' || key=='brand'){//如果点击的是分类或者是品牌
+		if(key=='category' || key=='brand' || key=='price'){//如果点击的是分类或者是品牌
 			$scope.searchMap[key]=value;
 		}else{ //用户点击的是规格
 			$scope.searchMap.spec[key]=value;
@@ -17,7 +17,7 @@ app.controller('searchController',function($scope,searchService){
 	
 	//撤销搜索项
 	$scope.removeSearchItem=function(key){
-		if(key=="category" ||  key=="brand"){//如果是分类或品牌
+		if(key=="category" ||  key=="brand" || key=='price'){//如果是分类或品牌
 			$scope.searchMap[key]="";		
 		}else{//否则是规格
 			delete $scope.searchMap.spec[key];//移除此属性
