ca482cc2b629f4ac8090b5a6d7fc7a982bc3c3e7
wangchong741 <853967228@qq.com>
2019-01-20 23:33:25 +0800

    搜索功能完成新品排序

--- pinyougou-pojo/src/main/java/com/pinyougou/pojo/TbItem.java
+++ pinyougou-pojo/src/main/java/com/pinyougou/pojo/TbItem.java
@@ -34,7 +34,8 @@ public class TbItem implements Serializable{
     private String status;
 
     private Date createTime;
-
+    
+    @Field("item_updatetime")
     private Date updateTime;
 
     private String itemSn;
