06bd174ebcb803af9607e37f14e394bc0321b59d
wangchong741 <853967228@qq.com>
2019-02-18 15:17:34 +0800

    支付日志

--- pinyougou-cart-web/src/main/java/com/pinyougou/cart/controller/PayController.java
+++ pinyougou-cart-web/src/main/java/com/pinyougou/cart/controller/PayController.java
@@ -1,64 +1,79 @@
 package com.pinyougou.cart.controller;
 
+import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.pinyougou.order.service.OrderService;
 import com.pinyougou.pay.service.WeiXinPayService;
+import com.pinyougou.pojo.TbPayLog;
 
 import entity.Result;
-import util.IdWorker;
 
 @RestController
 @RequestMapping("/pay")
 public class PayController {
-	
+
 	@Reference
 	private WeiXinPayService weixinPayService;
 	
-	
+	@Reference
+	private OrderService orderService;
+
 	@RequestMapping("/createNative")
-	public Map createNative(){
-		IdWorker idWorker = new IdWorker();
-		return weixinPayService.createNative(idWorker.nextId()+"", "1");
+	public Map createNative() {
+		// 1.获取当前登录用户
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+		// 2.提取支付日志（从缓存 ）
+		TbPayLog payLog = orderService.searchPayLogFromRedis(username);
+		// 3.调用微信支付接口
+		if (payLog != null) {
+			return weixinPayService.createNative(payLog.getOutTradeNo(), payLog.getTotalFee() + "");
+		} else {
+			return new HashMap<>();
+		}
 	}
 
 	/**
 	 * 检查支付状态
+	 * 
 	 * @param out_trade_no
 	 * @return
 	 */
 	@RequestMapping("/queryPayStatus")
-	public Result queryPayStatus(String out_trade_no){
-		Result result=null;		
-		int x=0;		
-		while(true){
+	public Result queryPayStatus(String out_trade_no) {
+		Result result = null;
+		int x = 0;
+		while (true) {
 			try {
-				Thread.sleep(5000);//间隔三秒
+				Thread.sleep(7000);// 7秒后自動支付成功(沒有商家賬號，模拟支付)
 			} catch (InterruptedException e) {
 				e.printStackTrace();
-			}	
-			//调用查询接口
-			Map<String,String> map = weixinPayService.queryPayStatus(out_trade_no);
-			if(map==null){//出错			
-				result=new  Result(false, "支付出错");
+			}
+			// 调用查询接口
+			Map<String, String> map = weixinPayService.queryPayStatus(out_trade_no);
+			if (map == null) {// 出错
+				result = new Result(false, "支付出错");
 				break;
-			}			
-			if(map.get("trade_state").equals("SUCCESS")){//如果成功				
-				result=new  Result(true, "支付成功");
+			}
+			if (map.get("trade_state").equals("SUCCESS")) {// 如果成功
+				result = new Result(true, "支付成功");
+				// 修改订单状态
+				orderService.updateOrderStatus(out_trade_no, map.get("transaction_id"));
 				break;
-			}			
-			//为了不让循环无休止地运行，我们定义一个循环变量，如果这个变量超过了这个值则退出循环，设置时间为5分钟
+			}
+			// 为了不让循环无休止地运行，我们定义一个循环变量，如果这个变量超过了这个值则退出循环，设置时间为5分钟
 			x++;
-			if(x>=100){
-				result=new  Result(false, "二维码超时");
+			if (x >= 100) {
+				result = new Result(false, "二维码超时");
 				break;
 			}
 		}
 		return result;
 	}
-	
-	
+
 }
--- pinyougou-order-service/src/main/java/com/pinyougou/order/service/impl/OrderServiceImpl.java
+++ pinyougou-order-service/src/main/java/com/pinyougou/order/service/impl/OrderServiceImpl.java
@@ -1,4 +1,5 @@
 package com.pinyougou.order.service.impl;
+
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
@@ -26,6 +27,7 @@ import util.IdWorker;
 
 /**
  * 服务实现层
+ * 
  * @author Administrator
  *
  */
@@ -35,10 +37,10 @@ public class OrderServiceImpl implements OrderService {
 
 	@Autowired
 	private TbOrderMapper orderMapper;
-	
+
 	@Autowired
 	private TbPayLogMapper payLogMapper;
-	
+
 	/**
 	 * 查询全部
 	 */
@@ -52,80 +54,101 @@ public class OrderServiceImpl implements OrderService {
 	 */
 	@Override
 	public PageResult findPage(int pageNum, int pageSize) {
-		PageHelper.startPage(pageNum, pageSize);		
-		Page<TbOrder> page=   (Page<TbOrder>) orderMapper.selectByExample(null);
+		PageHelper.startPage(pageNum, pageSize);
+		Page<TbOrder> page = (Page<TbOrder>) orderMapper.selectByExample(null);
 		return new PageResult(page.getTotal(), page.getResult());
 	}
 
 	@Autowired
 	private RedisTemplate redisTemplate;
-	
+
 	@Autowired
 	private IdWorker idWorker;
-	
+
 	@Autowired
 	private TbOrderItemMapper orderItemMapper;
-	
+
 	/**
 	 * 增加
 	 */
 	@Override
 	public void add(TbOrder order) {
-		
-		//1.从redis中提取购物车列表
-		List<Cart> cartList= (List<Cart>) redisTemplate.boundHashOps("cartList").get(order.getUserId());
-		
-		//2.循环购物车列表添加订单
-		for(Cart  cart:cartList){
-			TbOrder tbOrder=new TbOrder();
-			long orderId = idWorker.nextId();	//获取ID		
+
+		// 1.从redis中提取购物车列表
+		List<Cart> cartList = (List<Cart>) redisTemplate.boundHashOps("cartList").get(order.getUserId());
+
+		List<String> orderIdList = new ArrayList();// 订单ID列表
+		double total_money = 0;// 总金额 （元）
+
+		// 2.循环购物车列表添加订单
+		for (Cart cart : cartList) {
+			TbOrder tbOrder = new TbOrder();
+			long orderId = idWorker.nextId(); // 获取ID
 			tbOrder.setOrderId(orderId);
-			tbOrder.setPaymentType(order.getPaymentType());//支付类型
-			tbOrder.setStatus("1");//未付款 
-			tbOrder.setCreateTime(new Date());//下单时间
-			tbOrder.setUpdateTime(new Date());//更新时间
-			tbOrder.setUserId(order.getUserId());//当前用户
-			tbOrder.setReceiverAreaName(order.getReceiverAreaName());//收货人地址
-			tbOrder.setReceiverMobile(order.getReceiverMobile());//收货人电话
-			tbOrder.setReceiver(order.getReceiver());//收货人
-			tbOrder.setSourceType(order.getSourceType());//订单来源
-			tbOrder.setSellerId(order.getSellerId());//商家ID
-			
-			double money=0;//合计数
-			//循环购物车中每条明细记录
-			for(TbOrderItem orderItem:cart.getOrderItemList()  ){
-				orderItem.setId(idWorker.nextId());//主键
-				orderItem.setOrderId(orderId);//订单编号
-				orderItem.setSellerId(cart.getSellerId());//商家ID
-				orderItemMapper.insert(orderItem);				
-				money+=orderItem.getTotalFee().doubleValue();
+			tbOrder.setPaymentType(order.getPaymentType());// 支付类型
+			tbOrder.setStatus("1");// 未付款
+			tbOrder.setCreateTime(new Date());// 下单时间
+			tbOrder.setUpdateTime(new Date());// 更新时间
+			tbOrder.setUserId(order.getUserId());// 当前用户
+			tbOrder.setReceiverAreaName(order.getReceiverAreaName());// 收货人地址
+			tbOrder.setReceiverMobile(order.getReceiverMobile());// 收货人电话
+			tbOrder.setReceiver(order.getReceiver());// 收货人
+			tbOrder.setSourceType(order.getSourceType());// 订单来源
+			tbOrder.setSellerId(order.getSellerId());// 商家ID
+
+			double money = 0;// 合计数
+			// 循环购物车中每条明细记录
+			for (TbOrderItem orderItem : cart.getOrderItemList()) {
+				orderItem.setId(idWorker.nextId());// 主键
+				orderItem.setOrderId(orderId);// 订单编号
+				orderItem.setSellerId(cart.getSellerId());// 商家ID
+				orderItemMapper.insert(orderItem);
+				money += orderItem.getTotalFee().doubleValue();
 			}
-			
-			tbOrder.setPayment(new BigDecimal(money));//合计
+
+			tbOrder.setPayment(new BigDecimal(money));// 合计
 			orderMapper.insert(tbOrder);
+
+			orderIdList.add(orderId + "");// 添加到订单列表
+			total_money += money;// 累加到总金额
 		}
-		
-		
-		//3.清除redis中的购物车
+
+		if ("1".equals(order.getPaymentType())) {// 如果是微信支付
+			TbPayLog payLog = new TbPayLog();
+			String outTradeNo = idWorker.nextId() + "";// 支付订单号
+			payLog.setOutTradeNo(outTradeNo);// 支付订单号
+			payLog.setCreateTime(new Date());// 创建时间
+			// 订单号列表，逗号分隔
+			String ids = orderIdList.toString().replace("[", "").replace("]", "").replace(" ", "");
+			payLog.setOrderList(ids);// 订单号列表，逗号分隔
+			payLog.setPayType("1");// 支付类型
+			payLog.setTotalFee((long) (total_money * 100));// 总金额(分)
+			payLog.setTradeState("0");// 支付状态
+			payLog.setUserId(order.getUserId());// 用户ID
+			payLogMapper.insert(payLog);// 插入到支付日志表
+			redisTemplate.boundHashOps("payLog").put(order.getUserId(), payLog);// 放入缓存
+		}
+
+		// 3.清除redis中的购物车
 		redisTemplate.boundHashOps("cartList").delete(order.getUserId());
 	}
 
-	
 	/**
 	 * 修改
 	 */
 	@Override
-	public void update(TbOrder order){
+	public void update(TbOrder order) {
 		orderMapper.updateByPrimaryKey(order);
-	}	
-	
+	}
+
 	/**
 	 * 根据ID获取实体
+	 * 
 	 * @param id
 	 * @return
 	 */
 	@Override
-	public TbOrder findOne(Long id){
+	public TbOrder findOne(Long id) {
 		return orderMapper.selectByPrimaryKey(id);
 	}
 
@@ -134,103 +157,102 @@ public class OrderServiceImpl implements OrderService {
 	 */
 	@Override
 	public void delete(Long[] ids) {
-		for(Long id:ids){
+		for (Long id : ids) {
 			orderMapper.deleteByPrimaryKey(id);
-		}		
+		}
 	}
-	
-	
-		@Override
+
+	@Override
 	public PageResult findPage(TbOrder order, int pageNum, int pageSize) {
 		PageHelper.startPage(pageNum, pageSize);
-		
-		TbOrderExample example=new TbOrderExample();
+
+		TbOrderExample example = new TbOrderExample();
 		Criteria criteria = example.createCriteria();
-		
-		if(order!=null){			
-						if(order.getPaymentType()!=null && order.getPaymentType().length()>0){
-				criteria.andPaymentTypeLike("%"+order.getPaymentType()+"%");
+
+		if (order != null) {
+			if (order.getPaymentType() != null && order.getPaymentType().length() > 0) {
+				criteria.andPaymentTypeLike("%" + order.getPaymentType() + "%");
 			}
-			if(order.getPostFee()!=null && order.getPostFee().length()>0){
-				criteria.andPostFeeLike("%"+order.getPostFee()+"%");
+			if (order.getPostFee() != null && order.getPostFee().length() > 0) {
+				criteria.andPostFeeLike("%" + order.getPostFee() + "%");
 			}
-			if(order.getStatus()!=null && order.getStatus().length()>0){
-				criteria.andStatusLike("%"+order.getStatus()+"%");
+			if (order.getStatus() != null && order.getStatus().length() > 0) {
+				criteria.andStatusLike("%" + order.getStatus() + "%");
 			}
-			if(order.getShippingName()!=null && order.getShippingName().length()>0){
-				criteria.andShippingNameLike("%"+order.getShippingName()+"%");
+			if (order.getShippingName() != null && order.getShippingName().length() > 0) {
+				criteria.andShippingNameLike("%" + order.getShippingName() + "%");
 			}
-			if(order.getShippingCode()!=null && order.getShippingCode().length()>0){
-				criteria.andShippingCodeLike("%"+order.getShippingCode()+"%");
+			if (order.getShippingCode() != null && order.getShippingCode().length() > 0) {
+				criteria.andShippingCodeLike("%" + order.getShippingCode() + "%");
 			}
-			if(order.getUserId()!=null && order.getUserId().length()>0){
-				criteria.andUserIdLike("%"+order.getUserId()+"%");
+			if (order.getUserId() != null && order.getUserId().length() > 0) {
+				criteria.andUserIdLike("%" + order.getUserId() + "%");
 			}
-			if(order.getBuyerMessage()!=null && order.getBuyerMessage().length()>0){
-				criteria.andBuyerMessageLike("%"+order.getBuyerMessage()+"%");
+			if (order.getBuyerMessage() != null && order.getBuyerMessage().length() > 0) {
+				criteria.andBuyerMessageLike("%" + order.getBuyerMessage() + "%");
 			}
-			if(order.getBuyerNick()!=null && order.getBuyerNick().length()>0){
-				criteria.andBuyerNickLike("%"+order.getBuyerNick()+"%");
+			if (order.getBuyerNick() != null && order.getBuyerNick().length() > 0) {
+				criteria.andBuyerNickLike("%" + order.getBuyerNick() + "%");
 			}
-			if(order.getBuyerRate()!=null && order.getBuyerRate().length()>0){
-				criteria.andBuyerRateLike("%"+order.getBuyerRate()+"%");
+			if (order.getBuyerRate() != null && order.getBuyerRate().length() > 0) {
+				criteria.andBuyerRateLike("%" + order.getBuyerRate() + "%");
 			}
-			if(order.getReceiverAreaName()!=null && order.getReceiverAreaName().length()>0){
-				criteria.andReceiverAreaNameLike("%"+order.getReceiverAreaName()+"%");
+			if (order.getReceiverAreaName() != null && order.getReceiverAreaName().length() > 0) {
+				criteria.andReceiverAreaNameLike("%" + order.getReceiverAreaName() + "%");
 			}
-			if(order.getReceiverMobile()!=null && order.getReceiverMobile().length()>0){
-				criteria.andReceiverMobileLike("%"+order.getReceiverMobile()+"%");
+			if (order.getReceiverMobile() != null && order.getReceiverMobile().length() > 0) {
+				criteria.andReceiverMobileLike("%" + order.getReceiverMobile() + "%");
 			}
-			if(order.getReceiverZipCode()!=null && order.getReceiverZipCode().length()>0){
-				criteria.andReceiverZipCodeLike("%"+order.getReceiverZipCode()+"%");
+			if (order.getReceiverZipCode() != null && order.getReceiverZipCode().length() > 0) {
+				criteria.andReceiverZipCodeLike("%" + order.getReceiverZipCode() + "%");
 			}
-			if(order.getReceiver()!=null && order.getReceiver().length()>0){
-				criteria.andReceiverLike("%"+order.getReceiver()+"%");
+			if (order.getReceiver() != null && order.getReceiver().length() > 0) {
+				criteria.andReceiverLike("%" + order.getReceiver() + "%");
 			}
-			if(order.getInvoiceType()!=null && order.getInvoiceType().length()>0){
-				criteria.andInvoiceTypeLike("%"+order.getInvoiceType()+"%");
+			if (order.getInvoiceType() != null && order.getInvoiceType().length() > 0) {
+				criteria.andInvoiceTypeLike("%" + order.getInvoiceType() + "%");
 			}
-			if(order.getSourceType()!=null && order.getSourceType().length()>0){
-				criteria.andSourceTypeLike("%"+order.getSourceType()+"%");
+			if (order.getSourceType() != null && order.getSourceType().length() > 0) {
+				criteria.andSourceTypeLike("%" + order.getSourceType() + "%");
 			}
-			if(order.getSellerId()!=null && order.getSellerId().length()>0){
-				criteria.andSellerIdLike("%"+order.getSellerId()+"%");
+			if (order.getSellerId() != null && order.getSellerId().length() > 0) {
+				criteria.andSellerIdLike("%" + order.getSellerId() + "%");
 			}
-	
+
 		}
-		
-		Page<TbOrder> page= (Page<TbOrder>)orderMapper.selectByExample(example);		
+
+		Page<TbOrder> page = (Page<TbOrder>) orderMapper.selectByExample(example);
 		return new PageResult(page.getTotal(), page.getResult());
 	}
 
 	@Override
-	public TbPayLog searchPayLogFromRedis(String userId) {		
+	public TbPayLog searchPayLogFromRedis(String userId) {
 		return (TbPayLog) redisTemplate.boundHashOps("payLog").get(userId);
 	}
 
 	@Override
 	public void updateOrderStatus(String out_trade_no, String transaction_id) {
-		//1.修改支付日志的状态及相关字段
+		// 1.修改支付日志的状态及相关字段
 		TbPayLog payLog = payLogMapper.selectByPrimaryKey(out_trade_no);
-		payLog.setPayTime(new Date());//支付时间
-		payLog.setTradeState("1");//交易成功
-		payLog.setTransactionId(transaction_id);//微信的交易流水号
-		
-		payLogMapper.updateByPrimaryKey(payLog);//修改
-		//2.修改订单表的状态
+		payLog.setPayTime(new Date());// 支付时间
+		payLog.setTradeState("1");// 交易成功
+		payLog.setTransactionId(transaction_id);// 微信的交易流水号
+
+		payLogMapper.updateByPrimaryKey(payLog);// 修改
+		// 2.修改订单表的状态
 		String orderList = payLog.getOrderList();// 订单ID 串
 		String[] orderIds = orderList.split(",");
-		
-		for(String orderId:orderIds){
+
+		for (String orderId : orderIds) {
 			TbOrder order = orderMapper.selectByPrimaryKey(Long.valueOf(orderId));
-			order.setStatus("2");//已付款状态
-			order.setPaymentTime(new Date());//支付时间
-			orderMapper.updateByPrimaryKey(order);			
+			order.setStatus("2");// 已付款状态
+			order.setPaymentTime(new Date());// 支付时间
+			orderMapper.updateByPrimaryKey(order);
 		}
-		
-		//3.清除缓存中的payLog
+
+		// 3.清除缓存中的payLog
 		redisTemplate.boundHashOps("payLog").delete(payLog.getUserId());
-		
+
 	}
-	
+
 }
